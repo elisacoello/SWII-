1de9126939197b5c71223d0d971ef3ef3ee118e8
Carlos Palomo Rey <c.palomo1@usp.ceu.es>
2021-03-16 17:53:40 +0100

    XQuery y renombrada estructura xml

---
+++ Recetario/src/xml/RecetarioXQuery.java
@@ -0,0 +1,37 @@
+package xml;
+
+import com.saxonica.xqj.SaxonXQDataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
+import javax.xml.xquery.XQPreparedExpression;
+import javax.xml.xquery.XQResultSequence;
+
+public class RecetarioXQuery {
+	public static void main(String[] args) throws XQException {
+		File queryFile = new File("./recursos/prueba_XQuery.xq");
+
+		XQDataSource xqjd = new SaxonXQDataSource();
+		XQConnection xqjc = xqjd.getConnection();
+		InputStream inputStream = null;
+		try {
+		    inputStream = new FileInputStream(queryFile);
+		} catch (FileNotFoundException ex) {
+		    Logger.getLogger(TestXQuery.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		XQPreparedExpression exp = xqjc.prepareExpression(inputStream);
+		XQResultSequence result = exp.executeQuery();
+		while (result.next()) {
+		    System.out.println(result.getItemAsString(null));
+		}
+		result.close();
+		exp.close();
+		xqjc.close();
+	}
+}
